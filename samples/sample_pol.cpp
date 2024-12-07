2e0a5e4b02e0b851f685bd941ade6506f6b6c1be
Mamontyonok <kulikartur05@yandex.ru>
2024-12-08 02:14:42 +0300

    Update sample_pol.cpp

--- samples/sample_pol.cpp
+++ samples/sample_pol.cpp
@@ -31,18 +31,18 @@ int main()
     }
     cout << endl;
     cout << postfix_calculate(Polskaya(A.GetVector())) << endl;*/
-    string s = "35+(45-6)/11*56*(2*0)+134"; // -2
-    Translator A;
-    A.lexical_analysis(s);
+   // string s = "35+(45-6)/11*56*(2*0)+134"; // -2
+   // Translator A;
+  //  A.lexical_analysis(s);
    // vector v(A.GetVector());
-    vector v(Polskaya(A.GetVector()));
-    string s1 = ConvertForTests(v);
-    cout << s1;
-    string s2 = "35 45 6 - 11 / 56 * 2 0 * * + 134 + ";
-    cout << endl;
-    cout << s2;
-    if (s1 == s2)
-        cout << "good";
+//    vector v(Polskaya(A.GetVector()));
+//    string s1 = ConvertForTests(v);
+//    cout << s1;
+//    string s2 = "35 45 6 - 11 / 56 * 2 0 * * + 134 + ";
+//    cout << endl;
+//    cout << s2;
+  //  if (s1 == s2)
+  //      cout << "good";
     //for (int i = 0; i < A.GetVector().size(); i++)
      //   cout << (A.GetVector())[i]-> GetValue() << " ";
     
